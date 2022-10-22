338107a46fac37b96e5bb021dfaf63cc3337fd67
BananaFlamber <KorolevSteel@inbox.ru>
2022-10-22 21:29:01 +0300

    first task - done

--- 1.py
+++ 1.py
@@ -1,2 +1,18 @@
-number = int(input("Введите натуралное число для разложения на множители: "))
+print("\033c")
 
+import math 
+
+def fid_multiplier(numb): 
+    while numb % 2 == 0: 
+        print(2) 
+        numb = numb / 2 
+    for i in range(3, int(math.sqrt(numb)) + 1, 2): 
+        while numb % i == 0: 
+            print(i) 
+            numb = numb / i 
+    if numb > 2: 
+        print(numb) 
+ 
+numb = int(input("Введите натуралное число для его разложения на множители: ")) 
+
+fid_multiplier(numb) 
