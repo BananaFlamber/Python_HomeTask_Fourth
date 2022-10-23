98fe611464bbb2908eb1e7f6c7570ae3df1a2e64
BananaFlamber <KorolevSteel@inbox.ru>
2022-10-23 14:53:29 +0300

    third task - done

--- 3.py
+++ 3.py
@@ -1,12 +1,41 @@
-num = int(input("Введите число: "))
-i = 2 # первое простое число
-lst = []
-old = num
-while i <= num:
-    if num % i == 0:
-        lst.append(i)
-        num //= i
-        i = 2
+import random
+
+
+def write_file(st):
+    with open('result.txt', 'w') as data:
+        data.write(st)
+
+
+def rnd():
+    return random.randint(0,101)
+
+
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+    
+
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
     else:
-        i += 1
-print(f"Простые множители числа {old} приведены в списке: {lst}")
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+k = int(input("Введите натуральную степень k = "))
+koef = create_mn(k)
+write_file(create_str(koef))
